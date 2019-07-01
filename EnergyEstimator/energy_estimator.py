472cdd46c367f20fce79074ffafd7ed337b31e19
anle153 <anle@nii.ac.jp>
2019-07-01 13:21:23 +0900

    Add model training<

--- EnergyEstimator/energy_estimator.py
+++ EnergyEstimator/energy_estimator.py
@@ -1,7 +1,6 @@
 import matplotlib
 
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
@@ -10,13 +9,11 @@ from xgboost import XGBRegressor
 import common.configuration as Config
 from Models.LSTM_Model import lstm
 from common.utils import data_preprocessing, create_xy_set, file_exist
-from sklearn.metrics import mean_absolute_error, r2_score
+
 
 
 def build_network():
-    print '|--- Build lstm model.'
-    lstm_net = lstm(saving_path=Config.MODEL_SAVING_PATH + 'step-{}-hidden-{}/'.format(Config.N_TIMESTEPS,
-                                                                                       Config.HIDDEN_UNIT),
+    lstm_net = lstm(saving_path=Config.MODEL_SAVING_PATH,
                     input_shape=(Config.N_TIMESTEPS, Config.N_FEATURES),
                     hidden=Config.HIDDEN_UNIT,
                     drop_out=Config.DROP_OUT,
@@ -29,43 +26,38 @@ def build_network():
     return lstm_net
 
 
-def train(train_data):
-    """
-    Train LSTM-based model
-    :param train_data: tuple (train_x, train_y, valid_x, valid_y)
-    :return:
-    """
+def train(train_set, valid_set):
     print ('|--- Train average energy consumption predictor')
-
-    assert len(train_data) == 4
 
     lstm_net = build_network()
 
     if file_exist(lstm_net.saving_path + 'checkpoints/weights-{:02d}.hdf5'.format(Config.BEST_CHECKPOINT)):
         lstm_net.load_model_from_check_point(_from_epoch=Config.BEST_CHECKPOINT)
     else:
+        train_x, train_y = create_xy_set(train_set)
+        valid_x, valid_y = create_xy_set(valid_set)
 
         from_epoch = lstm_net.load_model_from_check_point()
         if from_epoch > 0:
             print('|--- Continue training forward model from epoch %i --- ' % from_epoch)
-            training_fw_history = lstm_net.model.fit(x=train_data[0],
-                                                     y=train_data[1],
+            training_fw_history = lstm_net.model.fit(x=train_x,
+                                                     y=train_y,
                                                      batch_size=Config.BATCH_SIZE,
                                                      epochs=Config.N_EPOCH,
                                                      callbacks=lstm_net.callbacks_list,
-                                                     validation_data=(train_data[2], train_data[3]),
+                                                     validation_data=(valid_x, valid_y),
                                                      shuffle=True,
                                                      initial_epoch=from_epoch,
                                                      verbose=2)
         else:
             print('|--- Training new forward model.')
 
-            training_fw_history = lstm_net.model.fit(x=train_data[0],
-                                                     y=train_data[1],
+            training_fw_history = lstm_net.model.fit(x=train_x,
+                                                     y=train_y,
                                                      batch_size=Config.BATCH_SIZE,
                                                      epochs=Config.N_EPOCH,
                                                      callbacks=lstm_net.callbacks_list,
-                                                     validation_data=(train_data[2], train_data[3]),
+                                                     validation_data=(valid_x, valid_y),
                                                      shuffle=True,
                                                      verbose=2)
         # Plot the training history
@@ -122,57 +114,23 @@ def test(raw_test_set, scaler):
         y_true[i] = test_set[i + n_tests]
 
 
-def xgb_test(data):
-    train_x, train_y = create_xy_set(data)
+if __name__ == '__main__':
+    raw_data = pd.read_csv(Config.RAW_DATA_PATH + 'log_file_noCharge_random.csv')
+    data = data_preprocessing(raw_data=raw_data.values)
+
+    train_set = data[:int(data.shape[0] * 0.8)]
+    test_set = data[int(data.shape[0] * 0.8):]
+
+    train_x, train_y = create_xy_set(train_set)
+    test_x, test_y = create_xy_set(test_set)
 
     train_x = train_x.squeeze(axis=2)
+    # train_y.squeeze(axis=1)
+    test_x = test_x.squeeze(axis=2)
+    # test_y.squeeze(axis=1)
 
     model = XGBRegressor(n_jobs=4)
     from sklearn.model_selection import cross_validate
 
-    cv_results = cross_validate(model, X=train_x, y=train_y, n_jobs=4, cv=10,
+    cv_results = cross_validate(model, X=train_x, y=train_y, n_jobs=4,
                                 scoring=("neg_mean_absolute_error", "neg_mean_squared_error"), return_train_score=True)
-
-    print cv_results.keys()
-
-    plt.plot(cv_results['train_neg_mean_absolute_error'], label='train_mae')
-    plt.plot(cv_results['test_neg_mean_absolute_error'], label='test_mae')
-    plt.legend()
-    plt.savefig('CV_plot.png')
-    plt.close()
-
-
-def lstm_train(train_set):
-    data_x, data_y = create_xy_set(train_set)
-
-    from sklearn.model_selection import train_test_split
-
-    train_x, valid_x, train_y, valid_y = train_test_split(data_x,data_y,test_size=0.2, shuffle=True)
-
-    model = train((train_x, train_y, valid_x, valid_y))
-
-    return model
-
-
-def lstm_test(test_set, lstm_net):
-    test_x, test_y = create_xy_set(test_set)
-
-    pred = lstm_net.model.predict(test_x)
-
-    mae = mean_absolute_error(y_pred=pred, y_true=test_y)
-    r2 = r2_score(y_true=test_y, y_pred=pred)
-
-    print "Results: MAE: {} --- R2: {}".format(mae, r2)
-
-
-if __name__ == '__main__':
-
-    raw_data = pd.read_csv(Config.RAW_DATA_PATH + 'log_file_noCharge_random.csv')
-    data = data_preprocessing(raw_data=raw_data.values)
-
-    train_set = data[:int(data.shape[0]*0.8)]
-    test_set = data[int(data.shape[0]*0.8):]
-
-    # xgb_test(train_set)
-    lstm_model = lstm_train(train_set)
-
